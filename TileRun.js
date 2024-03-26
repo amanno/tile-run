1dbdc42d2115dbe4e598e279245883629b0a4478
amanno <amanno5@yahoo.com>
2024-03-25 22:23:46 -0400

    Split out drawing logic from updating logic, and assigned colors to constants

--- TileRun.js
+++ TileRun.js
@@ -8,6 +8,11 @@ const NUM_TILES_HIGH = 10;
 
 const PATH_LENGTH = 15;
 
+const EMPTY_TILE = "gray";
+const UNTRAVERSED_PATH_TILE = "blue";
+const TRAVERSED_PATH_TILE = "green";
+const PLAYER_TILE = "yellow";
+
 document.onkeydown = checkKey;
 var direction;
 var currentPos = [0, 0];
@@ -34,21 +39,22 @@ function move(direction) {
     if (direction == "up") newPos = [currentPos[0], currentPos[1] - 1];
     if (direction == "right") newPos = [currentPos[0] + 1, currentPos[1]];
     if (direction == "down") newPos = [currentPos[0], currentPos[1] + 1];
-    if (JSON.stringify(newPos) == JSON.stringify(prevPos[prevPos.length - 1])) {
-        drawTile(currentPos[0], currentPos[1], "blue");
-        drawTile(prevPos[prevPos.length - 1][0], prevPos[prevPos.length - 1][1], "yellow");
+    if (JSON.stringify(newPos) == JSON.stringify(prevPos[prevPos.length - 1])) { // backtracking along path
+        updateTile(currentPos[0], currentPos[1], UNTRAVERSED_PATH_TILE);
+        updateTile(prevPos[prevPos.length - 1][0], prevPos[prevPos.length - 1][1], PLAYER_TILE);
         prevPos.pop();
-    } else if (isMovable(newPos[0], newPos[1])) {
-        drawTile(currentPos[0], currentPos[1], "green");
-        drawTile(newPos[0], newPos[1], "yellow");
+    } else if (isMovable(newPos[0], newPos[1])) { // all other movement
+        updateTile(currentPos[0], currentPos[1], TRAVERSED_PATH_TILE);
+        updateTile(newPos[0], newPos[1], PLAYER_TILE);
     }
 
     if (isVictory()) setup();
+    drawGame();
 }
 
-/* given a starting set of x/y coordinates and a path length, draws a path
+/* given a starting set of x/y coordinates and a path length, creates a path
     of the given length starting at the given set of coordinates */
-function drawPath(startX, startY, length) {
+function createPath(startX, startY, length) {
     var path = []; // path to be drawn
     var x = startX;
     var y = startY;
@@ -56,13 +62,14 @@ function drawPath(startX, startY, length) {
     for (var i = 0; i < length; i++) {
         path.push(findNextStep(x, y));
         nextStep = path[path.length - 1];
-        drawTile(x, y, "blue"); // draw current tile blue
+        updateTile(x, y, UNTRAVERSED_PATH_TILE); // draw current tile blue
         if (nextStep == undefined) break; // if path can't continue, end path here
         x = nextStep[0]; // set up x coordinate for next step
         y = nextStep[1]; // set up y coordinate for next step
     }
-    drawTile(startX, startY, "yellow"); // initiate starting position
+    updateTile(startX, startY, PLAYER_TILE); // initiate starting position
     prevPos = []; // clear out previous position
+    drawGame();
 }
 
 /* given a set of coordinates, find a random next step in the path to draw */
@@ -78,38 +85,49 @@ function findNextStep(x, y) {
 /* given a set of coordinates, returns true if a path can be drawn and false if it can't -
     for a path to be drawn, it must be in the board and be gray */
 function isValid(x, y) {
-    return x >= 0 && y >= 0 && x < board.length && y < board[x].length && board[x][y] == "gray";
+    return x >= 0 && y >= 0 && x < board.length && y < board[x].length && board[x][y] == EMPTY_TILE;
 }
 
 function isMovable(x, y) {
-    return x >= 0 && y >= 0 && x < board.length && y < board[x].length && board[x][y] != "gray" && (board[x][y] != "green" || prevPos[prevPos.length - 1] == [x, y]);
+    return x >= 0 && y >= 0 && x < board.length && y < board[x].length && board[x][y] != EMPTY_TILE && (board[x][y] != TRAVERSED_PATH_TILE || prevPos[prevPos.length - 1] == [x, y]);
 }
 
 function setup() {
     for (var i = 0; i < NUM_TILES_WIDE; i++) {
         for (var j = 0; j < NUM_TILES_HIGH; j++) {
-            drawTile(i, j, "gray");
+            updateTile(i, j, EMPTY_TILE);
         }
     }
-    drawPath(currentPos[0], currentPos[1], PATH_LENGTH);
+    createPath(currentPos[0], currentPos[1], PATH_LENGTH);
 }
 
 /* returns true if path has been cleared, returns false if path hasn't been cleared */
 function isVictory() {
     for (var i = 0; i < NUM_TILES_WIDE; i++) {
         for (var j = 0; j < NUM_TILES_HIGH; j++) {
-            if (board[i][j] == "blue") return false;
+            if (board[i][j] == UNTRAVERSED_PATH_TILE) return false;
         }
     }
     return true;
 }
 
+function updateTile(x, y, color) {
+    board[x][y] = color;
+    if (color == PLAYER_TILE) currentPos = [x, y];
+    if (color == TRAVERSED_PATH_TILE) prevPos.push([x, y]);
+}
+
 /* given a set of x/y coordinates and a color, draws the tile at those coordinates the given color */
 function drawTile(x, y, color) {
-    board[x][y] = color;
-    if (color == "yellow") currentPos = [x, y];
-    if (color == "green") prevPos.push([x, y]);
     var context = document.getElementById("canvas").getContext("2d");
     context.fillStyle = color;
     context.fillRect(x * (CANVAS_WIDTH / NUM_TILES_WIDE), y * (CANVAS_HEIGHT / NUM_TILES_HIGH), CANVAS_WIDTH / NUM_TILES_WIDE - 1, CANVAS_HEIGHT / NUM_TILES_HIGH - 1);
+}
+
+function drawGame() {
+    for (var i = 0; i < NUM_TILES_WIDE; i++) {
+        for (var j = 0; j < NUM_TILES_HIGH; j++) {
+            drawTile(i, j, board[i][j])
+        }
+    }
 }
